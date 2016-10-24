b262348bab0baaaa94871676cc42a92ac80bdf2e
xiico <francisco.marao@hotmail.com>
2016-10-24 17:56:16 -0200

    perfTest change

--- levels/perfTest.js
+++ levels/perfTest.js
@@ -2,7 +2,7 @@ var perfTest = {tiles:
 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n"+
 "X                      X╝      XX                                X\n"+
 "X   CX4   XXX                  ╚X                       ╔X       X\n"+
-"XCC CXXXXXX       X╗         X     X╗   X╗1             XX  X╗   X\n"+
+"XCC CXXXXXX       X╗         X     X╗   X╗1        p    XX  X╗   X\n"+
 "XCXXXXXXXXX C    XXX╗1       2     X ╗  XXX╗1    ╔X╗        22   X\n"+
 "X           C  XXXXXXX╗2           X  ╗ XXXXX╗1 ╔XXX╗            X\n"+
 "XXX    X4   XXXXXXXXXXXXX╗2        XXXXXXXXXXXXXXXXXX   X    X   X\n"+
@@ -10,7 +10,7 @@ var perfTest = {tiles:
 " XXXX   CC        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  X\n"+
 "      XXCC                                                       X\n"+
 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-
+movingPlatforms:[{id:"3-51", settings:{moving:true,movingOnX:true}}],
 backGround:
 "X X \n" +
 " X X\n" +
